25e4f201872444a2e8fc12500b0fa6e992654432
Eric Sorum <ejsorum@gmail.com>
2025-07-16 13:24:02 -0500

    Added modal click handlers.

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import Ingredients from "./Ingredients";
 import Instructions from "./Instructions";
 import style from '../style/RecipeCard.module.css';
 
+
+
 function RecipeCard({ recipe }) {
   const { image, id: recipeID, title } = recipe;
 
+  const cardRef = useRef(null);
 
   const expandCard = (e) => {
-    const thisEl = e.target;
+    const thisEl = e.target.closest("#expandableCard") || e.target;
     const thisAria = thisEl.getAttribute("aria-expanded")
     thisEl.setAttribute("aria-expanded",
       thisAria === "true" ? "false" : "true"
     )
   }
 
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      const cardEl = event.target.closest("#expandableCard");
+      if (!cardEl) {
+        cardRef.current.setAttribute("aria-expanded", "false")
+        
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <button type="button" className={style.RecipeCard} aria-expanded="false" onClick={expandCard}>
+    <button type="button" ref={cardRef} id="expandableCard" className={style.RecipeCard} aria-expanded="false" onClick={expandCard}>
       <p>{title}</p>
       <img src={image} alt={title} />
       <div className={style.ingredients}>
@@ -28,6 +46,8 @@ function RecipeCard({ recipe }) {
     </button>
   )
 }
+
+
 
 RecipeCard.defaultProps = {
   recipe: {
